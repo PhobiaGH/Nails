1910a9b50a48aa7da402ba195404157816b99862
PhobiaGH <phobiaghat@proton.me>
2023-08-14 22:21:02 -0400

    Added full page scrolling functionality

--- scrape.js
+++ scrape.js
@@ -24,6 +24,12 @@ async function scrape () {
     const browser = await puppeteer.launch({headless: "new"})
     const page = await browser.newPage(scrapePage)
     await page.goto(`${baseURL}`)
+    await page.setViewport({
+        width: 1920,
+        height: 1080
+    });
+
+    await autoScroll(page, 500)
     
     // Creates new dir, saves screenshot of site, all text inside page body, and all pictures on site
     var d = new Date()
@@ -44,6 +50,33 @@ async function scrape () {
         await fs.writeFile(`${dir}/${photo.split("/").pop()}`, await imagePage.buffer())
     }
     await browser.close()
+};
+
+// Auto scrolls down to load the entire page before extracting data
+async function autoScroll(page, maxScrolls) {
+    await page.evaluate(async (maxScrolls) => {
+        await new Promise((resolve) => {
+            var totalHeight = 0
+            var distance = 100
+            var scrolls = 0 // Scrolls counter
+            var timer = setInterval(() => {
+                var scrollHeight = document.body.scrollHeight
+                window.scrollBy(0, distance)
+                totalHeight += distance
+                scrolls++ // Incriment counter
+                
+                // Stops scrolling if end of page is reached, or maxScrolls is reached
+                try {
+                    if (totalHeight >= scrollHeight - window.innerHeight || scrolls >= maxScrolls) {
+                        clearInterval(timer)
+                        resolve()
+                    };
+                } catch (err) {
+                    console.log(`error with autoScroll: ${err.message}`)
+                };
+            }, 100)
+        });
+    }, maxScrolls); // Pass maxScrolls to the function
 };
 
 scrape();
